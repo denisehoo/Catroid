6085cd870f31458a67834e66c373dd85feedd42f
Thomas Kaufmann <kaufmaster@gmail.com>
2012-06-04 00:20:49 +0200

    7.32C: removed sleep

--- catroidUiTest/src/at/tugraz/ist/catroid/uitest/stage/InitializeStageLandscapeTest.java
+++ catroidUiTest/src/at/tugraz/ist/catroid/uitest/stage/InitializeStageLandscapeTest.java
@@ -56,7 +56,6 @@ public class InitializeStageLandscapeTest extends ActivityInstrumentationTestCas
 		solo.sleep(100);
 		solo.waitForActivity(MainMenuActivity.class.getSimpleName());
 		solo.setActivityOrientation(Solo.LANDSCAPE);
-		solo.sleep(200);
 		UiTestUtils.clickOnLinearLayout(solo, R.id.btn_action_play);
 		solo.waitForActivity(StageActivity.class.getSimpleName());
 		solo.sleep(500);
